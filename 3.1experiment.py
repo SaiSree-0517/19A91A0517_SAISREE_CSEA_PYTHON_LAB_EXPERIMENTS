54fd701e32e71f9cd09b778a2ec1064ffbfab03b
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-05-23 22:30:08 +0530

    Create 3.1experiment.py

---
+++ 3.1experiment.py
@@ -0,0 +1,14 @@
+"""
+3.1Implement a python script for checking whether the citizen is eligible for vote or not
+"""
+age=int(input("enter age :"))
+if age>=18:
+    print("Citizen is eligible for voting!")
+else:
+    print("Citizen not eligible for voting!")
+#output1
+enter age :19
+Citizen is eligible for voting!
+#output2
+enter age :15
+Citizen not eligible for voting!
