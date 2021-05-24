9c88d962f98fd72ffc75d29e58afa9d24caab6cb
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-05-24 11:17:19 +0530

    Update 5.3experiment.py

--- 5.3experiment.py
+++ 5.3experiment.py
@@ -1,4 +1,8 @@
-s= input("Enter the string: ")
+"""
+5.3) Implement a python script to count number of words
+in a string and reverse each word in a string at the same location.
+Example: Input :Honesty is the best policy Output :5 ytseno Hsiehttsebycilops= input("Enter the string: ")
+"""
 res = len(s.split())
 print ("The number of words in string are : " +  str(res))
 w = s.split(" ")
