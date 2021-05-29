888144c74d8535c1da928273d16ad86fb80e28fb
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-05-29 10:28:54 +0530

    Update 3.2experiment.py

--- 3.2experiment.py
+++ 3.2experiment.py
@@ -2,6 +2,7 @@
 3.2) Implement a python script using
 for loop that loops over a sequence
 """
+#for variable in range(start,end+1)
 start = int(input("Enter the start of range: "))
 end = int(input("Enter the end of range: "))  
 for num in range(start, end + 1):
@@ -12,3 +13,47 @@ for num in range(start, end + 1):
 Enter the start of range: 2
 Enter the end of range: 12
 2 4 6 8 10 12
+#for variable in string
+str="SAI SREE"
+for i in str:
+    print(i)
+#output
+S
+A
+I
+ 
+S
+R
+E
+E
+#for variable in list
+l=[1,2,3,4,5]
+for i in l:
+    print(i)
+#output
+1
+2
+3
+4
+5
+#for variablenin range(value)
+for i in range(6):
+    print(i)
+#output
+0
+1
+2
+3
+4
+5
+#for variable in range(start,end,incre/decre)
+for i in range(1,6,2):
+    print(i)
+#output
+1
+3
+5
+    
+
+
+    
